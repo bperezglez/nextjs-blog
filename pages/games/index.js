38cbac8c0640299c4397e0e29cd482811a2464bb
bperez <borjapgl@gmail.com>
2022-02-01 22:56:01 +0100

    reestructuracion del juego a su carpeta correspondiente

--- pages/games/index.js
+++ pages/games/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import Game from "../components/tic-tac-toe/game";
+import Layout, { siteTitle } from "../../components/layout";
+import Game from "./tic-tac-toe/game";
 import utilStyles from '../styles/utils.module.css'
 
 export default function Games() {
