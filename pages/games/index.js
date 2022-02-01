a2c23b5b0a7440e38ffec55724b37a18562e75db
bperez <borjapgl@gmail.com>
2022-02-01 23:02:59 +0100

    actualizar importacion del modulo de utils css

--- pages/games/index.js
+++ pages/games/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../../components/layout";
 import Game from "./tic-tac-toe/game";
-import utilStyles from '../styles/utils.module.css'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Games() {
   return (
